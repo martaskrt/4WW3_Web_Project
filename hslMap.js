3be543b0d3c40a793e74eeffdecd52a1387c174f
Marta Skreta <skretam@mcmaster.ca>
2018-03-05 04:26:47 -0500

    added a new JS file

--- hslMap.js
+++ hslMap.js
@@ -1,25 +1,23 @@
+	//Initializing HSL location
 	var myLatLng = {lat: 43.2602, lng: -79.9183};
 
-	var markers = [];
+	function initMap() {
+		//Instance new map object using Google Maps API; map is centered at myLatLng
+		var map = new google.maps.Map(document.getElementById('hslMap'), {
+			center: myLatLng,
+			zoom: 15
+		});
 
-function initMap() {
+		//Instance marker object that is positioned at myLatLng on the Google Maps object
+		var hslMarker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: 'Health Science Library'
+		});
 
-	var map = new google.maps.Map(document.getElementById('hslMap'), {
-		center: myLatLng,
-		zoom: 15
-	});
+		hslMarker.addListener('click', function() {
+			hslInfoWindow.open(map, hslMarker);
+		});
 
-	var hslMarker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: 'Health Science Library'
-	});
-
-	hslMarker.addListener('click', function() {
-		hslInfoWindow.open(map, hslMarker);
-	});
-
-	markers.push(hslMarker);
-
-}
+	}
 
