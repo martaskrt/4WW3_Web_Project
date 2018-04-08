a14e84e4ffef2bb7e7ead798d8711343536a28b0
Marta Skreta <skretam@mcmaster.ca>
2018-04-08 11:30:50 -0400

    js

--- liveMap.js
+++ liveMap.js
@@ -1,4 +1,4 @@
-	//Initializing location of center of map
+  	//Initializing location of center of map
 	var myLatLng = {lat: 43.2635, lng: -79.9180};
 
 	//Initializing library locations array
@@ -7,41 +7,23 @@
 	['Thode Library', 43.2611, -79.9225],
 	['Mills Library', 43.2627, -79.9177],
 	];
+	for (let library of JSON.parse(sql_global)){
+		locations.push([
+			library.libraryName, 
+			library.latitude,
+			library.longitude,
+			`<h2 class="library_name"><a href="sample/">` + library.libraryName + `</a></h2>
+			<div id="bodyContent">
+			<p>Mon-Thurs 8:45am-10:45pm
+			<br>Fri 8:45am-9:45pm
+			<br>Sat 10am-5:45pm
+			<br>Sun 8:45am-10:45pm</p>
+			</div>`
+			])
+	}
 	var markers = []; //initialize array which will be used for closing windows
 
 	//Initializing array of content to go in marker info boxes
-	var content = [
-
-	`<h2 class="library_name"><a href="sample/">Health Sciences Library</a></h2>
-	<div id="bodyContent">
-	<p><b>1280 Main St. W.</b></p>
-	<p>Mon-Thurs 8:45am-10:45pm
-	<br>Fri 8:45am-9:45pm
-	<br>Sat 10am-5:45pm
-	<br>Sun 8:45am-10:45pm</p>
-	</div>`,
-
-
-	`<h2 class="library_name">Thode Library</h2>
-	<div id="bodyContent">
-	<p><b>1280 Main St. W.</b></p>
-	<p>Mon-Thurs 8:45am-10:45pm
-	<br>Fri 8:45am-9:45pm
-	<br>Sat 10am-5:45pm
-	<br>Sun 8:45am-10:45pm</p>
-	</div>`,
-
-	`<h2 class="library_name">Mills Library</h2>
-	<div id="bodyContent">
-	<p><b>1280 Main St. W.</b></p>
-	<p>Mon-Thurs 8:45am-10:45pm
-	<br>Fri 8:45am-9:45pm
-	<br>Sat 10am-5:45pm
-	<br>Sun 8:45am-10:45pm</p>
-	</div>`
-
-	]
-
 	function initMap() {
 		//Instance new map object using Google Maps API; map is centered at myLatLng
 		var map = new google.maps.Map(document.getElementById('map'), {
@@ -58,7 +40,7 @@
 		for(i = 0; i < locations.length; i++){
 			//create new info window with contect from content array
 			var infoWindow = new google.maps.InfoWindow({
-				content: content[i],
+				content: locations[i][3],
 				maxWidth: 200
 			});
 			//create new marker on map objext with locations from locations aray
